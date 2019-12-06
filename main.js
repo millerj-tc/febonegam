eabb3768df3278be26e9efd9ac37d9d8bd9c1b65
millerj-tc <j.sam.miller@gmail.com>
2019-12-06 17:33:51 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var xmen = 528;
+var xmen = 533;
 window.onload = document.getElementById("span").innerHTML = xmen;
 window.onload = document.getElementById("buttonImg2").innerHTML = "<img src='rotatingArrow.gif' class='mark'>";
 window.onload = document.getElementById("buttonImg3").innerHTML = "<img src='arrow.png'>";

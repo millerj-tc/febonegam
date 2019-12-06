e13e67f3fef8c2469a28d235cafd6421b74e0c64
millerj-tc <j.sam.miller@gmail.com>
2019-12-06 17:52:21 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var xmen = 547;
+var xmen = 551;
 window.onload = document.getElementById("span").innerHTML = xmen;
 window.onload = document.getElementById("buttonImg2").innerHTML = "<img src='rotatingArrow.gif' class='mark'>";
 window.onload = document.getElementById("buttonImg3").innerHTML = "<img src='arrow.png'>";
@@ -10,8 +10,7 @@ function buttonSwitch() {
   console.log(document.getElementById("buttonImg").innerHTML);
   if(document.getElementById("buttonImg").innerHTML = "<img src='arrow.png'>") {
     document.getElementById("buttonImg").innerHTML =  "<img src='rotatingArrow.gif'>";      
-  }
-  else {
+  } else {
     console.log("elsed");
     document.getElementById("buttonImg").innerHTML =  "<img src='arrow.png'>";      
   }

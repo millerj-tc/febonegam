336067492298651835fbe4589a891592b1ed89f6
millerj-tc <j.sam.miller@gmail.com>
2019-12-05 22:16:23 -0600

    Update main.js

--- main.js
+++ main.js
@@ -3,5 +3,3 @@ function testClick() {
     document.getElementById("span").innerHTML = xmen;
 }
 document.getElementById("span").innerHTML = xmen;
-
-window.setInterval(function(){alert("BLORP"); }, 2000);

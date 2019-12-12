f7aeaf860a42236d7b338857e831ef410d8db796
millerj-tc <j.sam.miller@gmail.com>
2019-12-12 12:15:20 -0600

    Update passages.js

--- passages.js
+++ passages.js
@@ -1,2 +1,14 @@
+//PASSAGES
+class Passage {
+    constructor(storyID, text, progressbuttons) {
+        
+        this.storyID = storyID;
+        this.text = text;
+      this.progressbuttons = progressbuttons;
+        }
+}
+
+var grigory = new Passage(0, "Grigory was a shit", []);
+
 var wings = "rando";
-window.onload = document.getElementById("passages").innerHTML = "Brick";
+window.onload = document.getElementById("passages").innerHTML = "added Passages class";

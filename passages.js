52a47be5cd7c1f3305e8928d40f29485167c35bf
millerj-tc <j.sam.miller@gmail.com>
2019-12-13 12:23:31 -0600

    Update passages.js

--- passages.js
+++ passages.js
@@ -14,11 +14,11 @@ class Passage {
         }
 }
 
-//passage0 = new Passage(0, "Grigory was a penguin", passage1);
+passage0 = new Passage(0, "Grigory was a penguin", []);
 //passage1 = new Passage(1, "...but he was also a very big lion.", passage2);
 //passage2 = new Passage(2, "Practical.", passage3);
 //passage3 = new Passage(3, "Perfect.", passage4);
 
 
 var wings = "rando";
-window.onload = document.getElementById("passages").innerHTML = "passages 0-3, corrected to non-var";
+window.onload = document.getElementById("passages").innerHTML = "passages 0, empty third arg";

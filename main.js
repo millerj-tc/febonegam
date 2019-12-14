0530211a1d9c1e3807258e42a3fedd8754e8fc66
Joseph Miller <j.sam.miller@gmail.com>
2019-12-14 09:33:59 -0600

    1

--- main.js
+++ main.js
@@ -1,30 +1,22 @@
-
-
-//import { wings } from 'passages';
-
-//new Passage(tempPassID, tempPassText, tempPassButt)
-
-//passages
-
-//passages[0] = 
-
 var currentPassage = passage0;
 
 var xmen = "added next passage functionality, fixed ;, passage.text";
 window.onload = document.getElementById("main").innerHTML = xmen;
-window.onload = document.getElementById("passageDisplay").innerHTML = currentPassage.text;
+window.onload = document.getElementById("passageDisplay").innerHTML = currentPassage.ptext;
 
 
 var buttonRotation = 0;
+var buttonTimer =  [];
 
 function buttonSwitch() {
   if (buttonRotation == 0) {
       buttonRotation = 1;
       document.getElementById("buttonImg").innerHTML =  "<button class='button' onclick='buttonSwitch()'><img src='rotatingArrow.gif'></button>";
-      var buttonTimer = window.setInterval(resetButton, 805);
-      currentPassage = currentPassage.progessbuttons;
-      document.getElementById("passageDisplay").innerHTML = currentPassage.text;
+      buttonTimer = window.setInterval(resetButton, 650);
+      currentPassage = currentPassage.passageLinks;
+      document.getElementById("passageDisplay").innerHTML = currentPassage.ptext;
       } 
+}
     
  function resetButton() {
       buttonRotation = 0;
--- passages.js
+++ passages.js
@@ -2,23 +2,37 @@
 class Passage {
     constructor(passageStoryID, passageText, passageLinks) {
         
-        this.storyID = passageStoryID;
-        this.ptext = passageText;
-      this.progressbuttons = passageLinks;
+        this._storyID = passageStoryID;
+        this._ptext = passageText;
+        this._progressbuttons = passageLinks;
         }
-        get ptext() {
-            return this.ptext;   
-        }
-        get progressbuttons() {
-            return this.progressbuttons;   
-        }
+    get storyID() {
+        return this._storyID;
+    }
+    get ptext() {
+        return this._ptext;
+    }
+    set ptext(x) {
+        this._ptext = x;
+    }
+    get passageLinks() {
+        return this._passageLinks;
+    }
+    set passageLinks(x) {
+        this._passageLinks = x;
+    }
 }
 
-passage0 = new Passage(0, "Grigory was a penguin", 0);
-//passage1 = new Passage(1, "...but he was also a very big lion.", passage2);
-//passage2 = new Passage(2, "Practical.", passage3);
-//passage3 = new Passage(3, "Perfect.", passage4);
+passage0 = new Passage(0, "Grigory was a penguin", []);
+passage1 = new Passage(1, "...but he was also a very big lion.", []);
+passage2 = new Passage(2, "Practical.", []);
+passage3 = new Passage(3, "Perfect.", []);
 
+//passage mapping
+passage0.passageLinks = passage1;
+passage1.passageLinks = passage2;
+passage2.passageLinks = passage3;
 
 var wings = "rando";
-window.onload = document.getElementById("passages").innerHTML = "passages 0, empty third arg";
+window.onload = document.getElementById("passages").innerHTML = "passages 0, empty third arg, technically";
+window.onload = document.getElementById("passageDisplay").innerHTML = "test";

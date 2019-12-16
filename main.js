3811612aa6a86efcc1fddb58ddbc66a7dca7d6e6
millerj-tc <j.sam.miller@gmail.com>
2019-12-16 12:32:38 -0600

    Update main.js

--- main.js
+++ main.js
@@ -6,21 +6,37 @@ window.onload = document.getElementById("passageDisplay").innerHTML = currentPas
 
 
 var buttonRotation = 0;
+var tapOn = 0;
 var buttonTimer =  [];
 var rateTimer = [];
+var tapInterval = 0;
+var tapArray = [];
+var avgTapInterval = 0;
+var tapDataPoints = 0;
 
 function buttonSwitch() {
-  if (buttonRotation == 0) {
-      buttonRotation = 1;
-      document.getElementById("buttonImg").innerHTML =  "<button class='button' onclick='buttonSwitch()'><img src='rotatingArrow.gif'></button>";
-      buttonTimer = window.setInterval(resetButton, 650);
+  if (tapOn == 0) {
+      tapOn = 1;
+      //document.getElementById("buttonImg").innerHTML =  "<button class='button' onclick='buttonSwitch()'><img src='rotatingArrow.gif'></button>";
+      buttonTimer = window.setInterval(setRate, 500);
       currentPassage = currentPassage.passageLinks;
       document.getElementById("passageDisplay").innerHTML = currentPassage.ptext;
-      } 
+      }
+  if(tapOn == 1) {
+      //if(tapArray.length == 0) {
+      tapArray.push(tapInterval);  
+      //}
+      //if(tapArray > 0) {
+          //avgTapInterval = (avgTapInterval+tapInterval/60)/tapDataPoints;
+      //}
+      var x;
+      for (x of tapArray0 {
+          avgTapInterval = avgTapInterval + x; 
+          }
+      avgTapInterval = avgTapInterval/tapArray.length;
+      document.getElementById("rate").innerHTML = avgTapInterval;
 }
     
- function resetButton() {
-      buttonRotation = 0;
-     clearInterval(buttonTimer);
-      document.getElementById("buttonImg").innerHTML =  "<button class='button' onclick='buttonSwitch()'><img src='arrow.png'></button>";      
+ function setRate() {
+      tapInterval = tapInterval + .5;      
   }
